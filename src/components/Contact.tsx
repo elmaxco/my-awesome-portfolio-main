73f3639408f64785862e867a616d09028c0b3153
Max Jacobsson <Max.Jacobsson1999@hotmail.com>
2026-05-31 22:46:54 +0200

    Simplify contact icon hover animation

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -138,8 +138,8 @@ const Contact = () => {
             <motion.div 
               className="p-3 rounded-lg bg-primary/10 text-primary"
               animate={{ scale: 1, rotate: 0 }}
-              whileHover={{ scale: 1.1, rotate: [0, -10, 10, -10, 0] }}
-              transition={{ duration: 0.5, ease: "easeInOut" }}
+              whileHover={{ scale: 1.1 }}
+              transition={{ type: "spring", stiffness: 350, damping: 20 }}
             >
               <MapPin size={24} />
             </motion.div>
@@ -162,8 +162,8 @@ const Contact = () => {
             <motion.div 
               className="p-3 rounded-lg bg-primary/10 text-primary"
               animate={{ scale: 1, rotate: 0 }}
-              whileHover={{ scale: 1.1, rotate: [0, -10, 10, -10, 0] }}
-              transition={{ duration: 0.5, ease: "easeInOut" }}
+              whileHover={{ scale: 1.1 }}
+              transition={{ type: "spring", stiffness: 350, damping: 20 }}
             >
               <Mail size={24} />
             </motion.div>
